6f02ff9c7f7b2014f16f1e3131e2f5223f90e19b
huichuan1999 <58366430+huichuan1999@users.noreply.github.com>
2024-06-14 09:36:40 +0100

    update

---
+++ JavaScripts/butterflySketchMobile.js
@@ -0,0 +1,32 @@
+var sketch = function(p) {
+    let physics;
+    let butterfly;
+  
+    p.setup = function() {
+        let canvas = p.createCanvas(windowWidth, windowHeight);
+        canvas.parent('p5-ButterflyMobile');
+  
+        // 初始化物理系统
+        physics = new toxi.physics2d.VerletPhysics2D();
+        physics.setWorldBounds(new toxi.geom.Rect(0, 0, p.width, p.height));
+  
+        butterfly = new Butterfly(p.width / 2, p.height / 2, 50, p, physics);
+    }
+  
+    p.draw = function() {
+        // p.background(0);
+        p.clear();
+        p.noFill();
+        p.stroke(255);
+        p.strokeWeight(3);
+        p.rect(0,0,600,400);
+  
+        // 更新物理系统
+        physics.update();
+  
+        // 显示蝴蝶
+        butterfly.display();
+    }
+  }
+  
+  new p5(sketch);
