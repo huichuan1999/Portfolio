3e1ee8e353b355bfe59b01934bdb032dfb9b6021
Husheng Deng <husheng.deng@outlook.com>
2024-06-24 23:44:20 +0100

    further compression

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import { defineConfig } from "vite";
 export default defineConfig({
     build: {
         outDir: "dist",
-        minify: true,
+        minify: "esbuild",
+        terserOptions: {
+            compress: {
+                drop_console: true,
+            },
+            format: {
+                comments: false,
+            },
+            mangle: true,
+        }
     },
 });
