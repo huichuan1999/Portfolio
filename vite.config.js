0622b81aeff6227849569484879509839e4d5149
Husheng Deng <husheng.deng@outlook.com>
2024-06-25 09:03:22 +0100

    fix a part of hrefs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "/",
     build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                computationalArt: './computationalArtist/computationalArt.html',
+                ameobas: './computationalArtist/Amoebas.html',
+                codeincode: './computationalArtist/codeincode.html',
+                flyingBooks: './computationalArtist/flyingBooks.html',
+                noaa: './computationalArtist/NOAA.html',
+                randomFractals: './computationalArtist/RandomFractals.html',
+                theLensOfAi: './computationalArtist/TheLensOfAI.html',
+                about: "./about/aboutMe.html"
+            }
+        },
         outDir: "dist",
         minify: "esbuild",
         terserOptions: {
