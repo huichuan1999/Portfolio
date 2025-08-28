9373909d36495ba90fdee84a901f42edd542ec0a
huichuan1999 <huichuan1999@users.noreply.github.com>
2025-08-28 13:29:03 +0100

    update nav bar

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,12 @@ export function createNavbar() {
     return `
         <div class="nav">
             <ul class="navlist">
-                <li class="btli"><a href="/index.html">HOME</a></li>
+                <li class="btli">
+                    <a href="/index.html" class="home-icon-link">
+                        <!-- <img src="/assets/images/TangibleIllusions.jpg" alt="Home" class="home-icon" id="homeIcon"> -->
+                        <span class="home-text">Huichuan Wang</span>
+                    </a>
+                </li>
                 <li class="btli"><a href="/works.html">WORKS</a></li>
                 <li class="btli"><a href="/about/aboutMe.html">INFO</a>
                     <ul class="droplist">
@@ -14,5 +19,15 @@ export function createNavbar() {
                 </li>
             </ul>
         </div>
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                const homeIcon = document.getElementById('homeIcon');
+                if (homeIcon) {
+                    homeIcon.addEventListener('click', function() {
+                        this.src = '/assets/images/TangibleIllusions-1.png';
+                    });
+                }
+            });
+        </script>
     `;
 }
